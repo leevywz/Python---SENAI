1e5592c3919bdc7d206f6892db3af02cfb5e19c3
leevywz <a.levyneves@gmail.com>
2025-07-29 19:55:22 -0300

    Add files via upload

---
+++ aula5atividades.py
@@ -0,0 +1,41 @@
+n1 = 4 
+n2 = 6 
+
+r = (n1 * n2)/2
+
+print('Resultado;', r)
+
+r = (n1 * n2)/2
+
+n1 = 5
+n2 = 7
+
+soma = n1 + n2
+
+print('Resultado:', soma)
+
+n1 = 3
+r = n1 ** 4
+print ('Resultado', r)
+
+n1 = 8
+n2 = 12
+n3 = 15
+
+media = (8 + 12 + 15)/3
+
+print ('Resultado', round(media))
+
+n1 = 10
+n2 = 2
+
+r = (n2 - n1) * 5
+
+print ('Resultado', r)
+
+n1 = 27
+n2 = 3
+
+r = n1 / n2 + 5
+
+print ('Resultado', r)
