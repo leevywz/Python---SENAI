478ad5868be50c357c764d4eea9a24626f3f6d7e
leevywz <a.levyneves@gmail.com>
2025-07-29 19:30:22 -0300

    Add files via upload

---
+++ Aula 5.py
@@ -0,0 +1,37 @@
+# lista  =  [1,20,31,45,50,61]
+# lista[1] = 200
+# print(lista)
+
+
+# soma  =  lista[2] + lista[-1]
+# print(soma)
+
+
+# isso_e_uma_lista = list(range(1,251,2))
+# print(isso_e_uma_lista)
+
+
+# l = list(range(1,11))
+# print(l)
+
+
+# funções:
+
+
+# funções para adicionar um dado
+l = []
+print(l)
+l.append(10)
+print(l)
+l.extend([10,1,2,0])
+l +=(5,332,65)
+l.insert(0,250)
+l[0] = 250
+print(l)
+
+
+# deleta o último valor
+l.pop(1)
+l.remove(250)
+del l[3]
+print(l)
